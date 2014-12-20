e3c8ed6adb1c4b514ff2ccced5674f443469b91f
Kurt Smith <ksmith@enthought.com>
2014-12-19 23:12:29 -0600

    Tweaks to output.

--- 12-parallel-cython/02-prange-parallel-loops/run_julia.py
+++ 12-parallel-cython/02-prange-parallel-loops/run_julia.py
@@ -1,9 +1,13 @@
 import julia
 import numpy as np
+from time import clock
 import matplotlib.pyplot as plt
 
+t1 = clock()
 jl = julia.calc_julia(1000, (0.322 + 0.05j))
-print julia.julia_fraction(jl)
+print "time:", clock() - t1
+
+print "julia fraction:", julia.julia_fraction(jl)
 
 plt.imshow(np.log(jl))
 plt.show()
